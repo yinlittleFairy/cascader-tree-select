daf84aaf513f0f3e172882c253f32f2a01b7331e
yinyueshuang <yinyueshuang@jd.com>
2023-08-21 21:16:51 +0800

    chore: update package

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,39 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue2'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueSetupExtend()],
-  test: {
-    environment: 'happy-dom'
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  build: {
-    outDir: 'lib',
-    lib: {
-      entry: resolve(__dirname, "src/index.js"),
-      name: 'cascaderTreeSelect',
-      fileName: (format) => `cascader-tree-select.${format}.js`
+
+export default ({ mode }) => {
+  console.log(mode, 'mode')
+  const isLib = mode === 'lib'
+
+  const basic = {
+    plugins: [vue(), VueSetupExtend()],
+    test: {
+      environment: 'happy-dom'
     },
-    rollupOptions: {
-      external: ['vue', 'element-ui']
-    }
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+
   }
-})
+
+  if (isLib) Object.assign(basic, {
+    build: {
+      outDir: 'lib',
+      lib: {
+        entry: resolve(__dirname, "src/index.js"),
+        name: 'cascaderTreeSelect',
+        fileName: (format) => `cascader-tree-select.${format}.js`
+      },
+      rollupOptions: {
+        external: ['vue', 'element-ui']
+      }
+    }
+  })
+
+  return defineConfig(basic)
+
+}
