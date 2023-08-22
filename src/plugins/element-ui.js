6b91774eb1f646d4e4bcdeff9587c40adada761c
yinyueshuang <yinyueshuang@jd.com>
2023-08-22 16:20:23 +0800

    test: 第三方库测试

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -3,20 +3,20 @@ import {
   Input,
   Checkbox,
   Col,
-  Button,
   Form,
   FormItem,
   Radio,
   RadioGroup,
-  Row
+  Row,
+  Button
 } from 'element-ui'
 
 Vue.use(Input)
 Vue.use(Col)
 Vue.use(Checkbox)
-Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(RadioGroup)
 Vue.use(Radio)
 Vue.use(Row)
+Vue.use(Button)
