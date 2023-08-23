4cf025aa2acede9d81b25160b0505f1f1d6c1182
yinyueshuang <yinyueshuang@jd.com>
2023-08-23 18:06:51 +0800

    fix: 增加docs

--- node_modules/happy-dom/src/query-selector/SelectorCombinatorEnum.ts
+++
@@ -1,7 +0,0 @@
-enum SelectorCombinatorEnum {
-	descendant = 'descendant',
-	child = 'child',
-	adjacentSibling = 'adjacentSibling'
-}
-
-export default SelectorCombinatorEnum;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import CascaderTreeSelect from "./index.vue";
-
-CascaderTreeSelect.install = function (app) {
-  app.component(CascaderTreeSelect.name, CascaderTreeSelect);
-};
-export default CascaderTreeSelect;
