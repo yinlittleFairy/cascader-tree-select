7313da00132978a8e171cbc215317d998acf5dff
yinyueshuang <yinyueshuang@jd.com>
2023-08-23 11:24:23 +0800

    chore: 移除多余文件

--- node_modules/happy-dom/lib/config/PlainTextElements.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: {
-    STYLE: boolean;
-    SCRIPT: boolean;
-};
-export default _default;
-//# sourceMappingURL=PlainTextElements.d.ts.map
--- node_modules/happy-dom/lib/query-selector/SelectorCombinatorEnum.d.ts
+++
@@ -1,7 +0,0 @@
-declare enum SelectorCombinatorEnum {
-    descendant = "descendant",
-    child = "child",
-    adjacentSibling = "adjacentSibling"
-}
-export default SelectorCombinatorEnum;
-//# sourceMappingURL=SelectorCombinatorEnum.d.ts.map
